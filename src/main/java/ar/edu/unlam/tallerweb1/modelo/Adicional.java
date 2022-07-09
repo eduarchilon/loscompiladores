06a23b9d08ef10017419d1dccc0b20f42d965776
eduar <eduarchilon@gmail.com>
2022-07-08 23:44:47 -0300

    feat: add class

--- src/main/java/ar/edu/unlam/tallerweb1/modelo/Adicional.java
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/Adicional.java
@@ -15,4 +15,38 @@ public class Adicional {
 
     @Column(name = "precio")
     private Double precio;
+
+    public Adicional(Long id, String nombre, Double precio) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public Adicional() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
 }
